0bc7ca1ba549a2024bfb04d4ea046b4c70284c59
xubing-PC\xubing <876313557@qq.com>
2018-03-06 21:12:01 +0800

    我的第一次提交哟

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * 
+ */
+public class Main {
+
+     private static final  String HELLO="hello";
+    public static void main(String[] args) {
+	    System.out.print(Main.HELLO);private
+
+        System.out.print("1");
+
+
+
+
+    }
+}
